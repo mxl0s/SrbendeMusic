40a145baf8c37e9c3e249b21e734a28d63eff589
mxl0s <75951702+mxl0s@users.noreply.github.com>
2021-05-17 04:28:55 +0200

    prettier embed

--- Economy/shop.js
+++ Economy/shop.js
@@ -10,13 +10,10 @@ module.exports = {
 
 	async execute(message, args) {
      let embed = new Discord.MessageEmbed()
-    .setTitle("Pegaz")
-    .addField('Cat (Big Floppa) — **1000**', `Uzmi Floppu, i daruje ti 1000 coinsa dnevno!`)
-    .addField('Kola — **5000**', `Koristi kola da gaziš pešake! I zaradi do 10000 coinsa!`)
-    .addField('Villa — **20000**', `Kupi Villu i iznajmi je stanarima, zaradi od 2000 do 30000 coinsa nedeljno!`)
-    .addField('Minecraft — **50**', `Igraj MC i zaradi!`)
+    .setAuthor("Srbende Music | Shop 🛒,", "https://cdn.discordapp.com/app-icons/802583582022697011/817941229e62f9dc8f6219ab6ef21a10.png")
+    .setDescription('Cat (Big Floppa) — **1000**\nUzmi Floppu, i daruje ti 1000 coinsa dnevno!\nKola — **5000** Koristi kola da gaziš pešake!\n I zaradi do 10000 coinsa!\nVilla — **20000**\nKupi Villu i iznajmi je stanarima, zaradi od 2000 do 30000 coinsa nedeljno!\nMinecraft — **50**\nIgraj MC i zaradi!')
     .setColor("#3371FF")
-	.setThumbnail("http://pegaz-ruma.com/img/pekara_i_picerija_Ruma_Pegaz_pekarski_i_poslasti%C4%8Darski_proizvodi_dostava_Ruma_Sremska_Mitrovica_LOGO.jpg")
+	  .setThumbnail("https://cdn.discordapp.com/app-icons/802583582022697011/817941229e62f9dc8f6219ab6ef21a10.png")
     .setFooter("Kucaj !buy [ime stvari] da je kupiš! Da je koristiš, !use [stvar]")
     message.channel.send(embed)
 

28cb7df8c5d1597ca421e8aaaf555b1047374a44
mxl0s <75951702+mxl0s@users.noreply.github.com>
2021-05-17 04:24:01 +0200

    pretty embeds cleaned code

--- Economy/give.js
+++ Economy/give.js
@@ -15,19 +15,38 @@ module.exports = {
 
 		let person = message.mentions.members.first(message, args[0]);
 
+    const bruh = new Discord.MessageEmbed()
+    .setAuthor('SM | Admin Pay ⛏️', 'https://cdn.discordapp.com/app-icons/802583582022697011/817941229e62f9dc8f6219ab6ef21a10.png')
+    .setThumbnail("https://cdn.discordapp.com/app-icons/807706628278583346/c7df1f389b00c9c0f58ebbf162956e2b.png")
+    .setDescription(`Nisi kreator!`)
+    .setColor("#3371FF")
+
+    const bruh2 = new Discord.MessageEmbed()
+    .setAuthor('SM | Admin Pay ⛏️', 'https://cdn.discordapp.com/app-icons/802583582022697011/817941229e62f9dc8f6219ab6ef21a10.png')
+    .setThumbnail("https://cdn.discordapp.com/app-icons/807706628278583346/c7df1f389b00c9c0f58ebbf162956e2b.png")
+    .setDescription(`Unesi membera!`)
+    .setColor("#3371FF")
+
+    const bruh3 = new Discord.MessageEmbed()
+    .setAuthor('SM | Admin Pay ⛏️', 'https://cdn.discordapp.com/app-icons/802583582022697011/817941229e62f9dc8f6219ab6ef21a10.png')
+    .setThumbnail("https://cdn.discordapp.com/app-icons/807706628278583346/c7df1f389b00c9c0f58ebbf162956e2b.png")
+    .setDescription(`Unesi Količinu!`)
+    .setColor("#3371FF")
+
+
   let user = message.mentions.users.first() 
-if(message.author.id !== "711272428499632169") return message.inlineReply('Samo kreator može ovo koristiti!')
+if(message.author.id !== "711272428499632169") return message.channel.send(bruh)
   let member = db.fetch(`money_${message.author.id}`)
 
  ;
 
   if (!user) {
-      return message.inlineReply(`Unesi membera!`, { allowedMentions: { repliedUser: false } })
+      return message.channel.send(bruh2)
   }
 
   
   if (!args[1]) {
-      return message.inlineReply('Unesi količinu.')
+      return message.cahnnel.send(bruh3)
   }
   
   let embed5 = new Discord.MessageEmbed()

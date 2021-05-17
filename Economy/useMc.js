0b7effeb1b6a56f5ad7a221f2f605798393b21d0
mxl0s <75951702+mxl0s@users.noreply.github.com>
2021-05-17 04:37:46 +0200

    wow guess what pretty embed woo

--- Economy/useMc.js
+++ Economy/useMc.js
@@ -13,7 +13,23 @@ module.exports = {
 	async execute(message, args) {
 let user = message.author;
 
- 
+const bruh1 = new Discord.MessageEmbed()
+.setAuthor('Srbende Music | ⛏️ MC', 'https://cdn.discordapp.com/app-icons/802583582022697011/817941229e62f9dc8f6219ab6ef21a10.png')
+.setThumbnail("https://cdn.discordapp.com/app-icons/807706628278583346/c7df1f389b00c9c0f58ebbf162956e2b.png")
+.setDescription(`Moraš Kupiti MC iz Shopa!`)
+.setColor("#3371FF")
+
+const bruh2 = new Discord.MessageEmbed()
+.setAuthor('Srbende Music | ⛏️ MC', 'https://cdn.discordapp.com/app-icons/802583582022697011/817941229e62f9dc8f6219ab6ef21a10.png')
+.setThumbnail("https://cdn.discordapp.com/app-icons/807706628278583346/c7df1f389b00c9c0f58ebbf162956e2b.png")
+.setDescription(`Uspori, već si igrao MC, vrati se za ${time.seconds}s`)
+.setColor("#3371FF")
+
+const bruh3 = new Discord.MessageEmbed()
+.setAuthor('Srbende Music | ⛏️ MC', 'https://cdn.discordapp.com/app-icons/802583582022697011/817941229e62f9dc8f6219ab6ef21a10.png')
+.setThumbnail("https://cdn.discordapp.com/app-icons/807706628278583346/c7df1f389b00c9c0f58ebbf162956e2b.png")
+.setDescription(`Igrao si MC celu noć. Bilo je toliko dobro da si sad **${rand}** coinsa bogatiji! `)
+.setColor("#3371FF")
 
 if (args[0] == 'minecraft' || args[0] == 'mc') {
  let timeout = 120000;
@@ -23,12 +39,11 @@ let mc = await db.fetch(`mc_${user.id}`)
   if (mc !== null && timeout - (Date.now() - mc) > 0) {
     let time = ms(timeout - (Date.now() - mc));
   
- message.inlineReply(`Šefe, uspori. Već si igrao MC celu noć sinoć, zar opet?! Vrati se za **${time.seconds}s**`, { allowedMentions: { repliedUser: false } })
+ message.channel.send(bruh2)
   } else {
-
   let mc = await db.fetch(`mc_${user.id}`)
-  if(mc === null) return message.inlineReply("Moraš kupiti MC iz shopa!")
-  if(mc > 0) message.channel.send(`Igrao si MC celu noć. Bilo je toliko dobro da si sad **${rand}** coinsa bogatiji! `)
+  if(mc === null) return message.channel.send(bruh1)
+  if(mc > 0) message.channel.send(bruh3)
    db.add(`money_${user.id}`, rand)
    db.set(`mc_${user.id}`, Date.now())
 }

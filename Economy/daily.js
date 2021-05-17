1de5dd0da08ee9fd67df5235330a1652f84da983
mxl0s <75951702+mxl0s@users.noreply.github.com>
2021-05-17 04:17:33 +0200

    added new embed

--- Economy/daily.js
+++ Economy/daily.js
@@ -18,6 +18,12 @@ module.exports = {
   let timeout = 86400000;
   let amount = 1500;
 
+  const dailyembed = new Discord.MessageEmbed()
+  .setAuthor('Srbende Music | 🎁 Daily', 'https://cdn.discordapp.com/app-icons/802583582022697011/817941229e62f9dc8f6219ab6ef21a10.png')
+  .setThumbnail("https://cdn.discordapp.com/app-icons/807706628278583346/c7df1f389b00c9c0f58ebbf162956e2b.png")
+  .setDescription(`Pokupio si Daily od ${coins} coinsa!`)
+  .setColor("#3371FF")
+
   let daily = await db.fetch(`daily_${user.id}`);
 
   if (daily !== null && timeout - (Date.now() - daily) > 0) {
@@ -34,7 +40,7 @@ module.exports = {
 });
   } else {
    
-  message.inlineReply(`Pokupio si daily od **${amount}** Coinsa!`, { allowedMentions: { repliedUser: false } })
+  message.channel.send(dailyembed);
   db.add(`money_${user.id}`, amount)
   db.set(`daily_${user.id}`, Date.now())
 
